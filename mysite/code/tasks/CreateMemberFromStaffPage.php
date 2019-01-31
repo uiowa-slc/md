190b82a08c9cd9d8f1dde693fa094eefbaaa2eb3
Jessica Brown <jessica-e-brown@uiowa.edu>
2019-01-31 10:59:57 -0600

    Create CreateMemberFromStaffPage.php

---
+++ mysite/code/tasks/CreateMemberFromStaffPage.php
@@ -0,0 +1,45 @@
+<?php
+use Silverstripe\Dev\BuildTask;
+use Silverstripe\Security\Member;
+class CreateMemberFromStaffPage extends BuildTask {
+ 
+    protected $title = 'Create Members from StaffPages';
+ 
+    protected $description = 'Create Member associated with StaffPage, if none exist, and/or update the StaffPage memberID';
+ 
+    protected $enabled = true;
+ 
+    function run($request) {
+        $staffPages = StaffPage::get(); //get all staff pages
+        foreach($staffPages as $staff){
+            $memberID = $staff->MemberID;
+            $email = $staff->EmailAddress;
+            // print_r($email);
+            if($memberID == 0){
+                $memberTest = Member::get()->filter(array('Email' => $email))->First();
+                // print_r($memberTest);
+                if(!$memberTest){
+                    $member = new Member();
+                    $member->Email = $staff->EmailAddress;
+                    $staff->MemberID = $member->ID;
+                    $member->write();
+                    $staff->write();
+                    print_r('<li>created member and updated staff memberID</li>');
+                }
+                else{
+                    $existingMember = Member::get()->filter(array('Email' => $email))->First();
+                    // print_r($existingMember->ID);
+                    $staff->MemberID = $existingMember->ID;
+                    $staff->write();
+                    print_r('<li>updated staff memberID</li>');
+                }
+            }
+            else{
+                $existingMember = Member::get()->filter(array('Email' => $email))->First();
+                $staff->MemberID = $existingMember->ID;
+                print_r('<li>updated staff member ID</li>');
+            }
+            
+        }
+    }
+}
