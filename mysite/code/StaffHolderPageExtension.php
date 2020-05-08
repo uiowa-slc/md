6525ca8b0a476261df3a0c53cebd511d6f4316dc
Dustin Quam <quamsta@gmail.com>
2020-05-08 11:11:53 -0500

    updated modules added things

--- mysite/code/StaffHolderPageExtension.php
+++ mysite/code/StaffHolderPageExtension.php
@@ -3,6 +3,8 @@
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\GridField\GridFieldDataColumns;
+use SilverStripe\Lumberjack\Model\Lumberjack;
 class StaffHolderPageExtension extends DataExtension {
 
 	private static $db = array(
@@ -12,7 +14,35 @@ class StaffHolderPageExtension extends DataExtension {
 	);
 
 	public function updateCMSFields(FieldList $fields){
-		$fields->addFieldToTab('Root.Main', new HTMLEditorField('AlumniContent', 'Content to be displayed on the alumni page.'));
+		//$fields->addFieldToTab('Root.Main', new HTMLEditorField('AlumniContent', 'Content to be displayed on the alumni page.'));
+
+        $fields->removeByName('Content');
+        $fields->removeByName('Content');
+        $fields->removeByName('Widgets');
+        $fields->removeByName('Dependent');
+        $fields->removeByName('LayoutType');
+        $grid = $fields->dataFieldByName('ChildPages');
+
+        $config = $grid->getConfig();
+        $dataColumns = $config->getComponentByType(GridFieldDataColumns::class);
+
+        $dataColumns->setDisplayFields([
+            'Title' => 'Title',
+            'LastEdited' => 'Last Edited'
+        ]);
+
+        $list = $grid->getList();
+
+        $sortedList = $list->sort('LastEdited DESC');
+
+        $grid->setList($sortedList);
+        $grid->setTitle('Staff Members');
+        $grid->setName('StaffPages');
+
+
+
+        $fields->addFieldToTab('Root.Main', $grid);
+        $fields->removeByName('ChildPages');
 	}
 
 }
